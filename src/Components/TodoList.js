42a683b8127d9ef3736918b87b76fd7ede571d4b
Atre Pramod Namdev <pramodatre05@gmail.com>
2023-05-01 09:16:35 +0530

    done 212 change theme with button

--- src/Components/TodoList.js
+++ src/Components/TodoList.js
@@ -3,7 +3,8 @@ import { ThemeContext } from '../contexts/ThemeContext'
 export class TodoList extends React.Component {
     static contextType = ThemeContext;
     render() {
-        const { isDarkTheme, darkTheme, lightTheme } = this.context;
+
+        const { isDarkTheme, darkTheme, lightTheme, changeTheme } = this.context;
         const theme = isDarkTheme ? darkTheme : lightTheme;
 
         return (
@@ -13,6 +14,7 @@ export class TodoList extends React.Component {
                 <p className='items'>Paln the family trip</p>
                 <p className='items'> Go to shopping </p>
                 <p className='items'>Go to walk</p>
+                <button className='ui button primary' onClick={changeTheme}>Change the theme</button>
 
             </div>
         )
--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -5,7 +5,7 @@ export const ThemeContext = createContext();
 
 class ThemeContextProvider extends React.Component {
     state = {
-        isDarkTheme: false,
+        isDarkTheme: true,
         lightTheme: {
             text: '#222',
             background: '#d8ddf1'
@@ -15,9 +15,12 @@ class ThemeContextProvider extends React.Component {
             background: '#5c5c5c'
         }
     };
+    changeTheme = () => {
+        this.setState({ isDarkTheme: !this.state.isDarkTheme })
+    };
     render() {
         return (
-            <ThemeContext.Provider value={{ ...this.state }}>
+            <ThemeContext.Provider value={{ ...this.state, changeTheme: this.changeTheme }}>
                 {this.props.children}
             </ThemeContext.Provider>
         )
